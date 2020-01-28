bed4c3573bc5f6d9e530e3aa8f61f85f974c32be
Ryan Ma <ryanyma@umich.edu>
2020-01-28 14:17:29 -0500

    set up react with graphql and apollo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import Routes from "./routes";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "http://localhost:8080/graphql"
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/routes/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+function Home() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      allUsers {
+        id
+        email
+      }
+    }
+  `);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+  console.log(data);
+  return data.allUsers.map(({ id, email }) => <h1 key={id}>{email}</h1>);
+}
+
+export default Home;
